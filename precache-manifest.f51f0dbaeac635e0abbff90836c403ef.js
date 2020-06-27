39cb816f346f6e9e9db9b042d43e00a1df86411e
Robert Vo <56901201+RobertVo93@users.noreply.github.com>
2020-06-27 23:35:57 +0700

    Updates

---
+++ precache-manifest.f51f0dbaeac635e0abbff90836c403ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63264a62cbc0de65cbfc5c53312653ec",
+    "url": "https://RobertVo93.github.io/activitylogs/index.html"
+  },
+  {
+    "revision": "7affb3a24f79d1d1aaea",
+    "url": "https://RobertVo93.github.io/activitylogs/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "a16e9b8287d82d7faad0",
+    "url": "https://RobertVo93.github.io/activitylogs/static/css/main.f1a00c2f.chunk.css"
+  },
+  {
+    "revision": "7affb3a24f79d1d1aaea",
+    "url": "https://RobertVo93.github.io/activitylogs/static/js/2.a6f894ed.chunk.js"
+  },
+  {
+    "revision": "14ca31ac961bff64dbe91ab817a1f176",
+    "url": "https://RobertVo93.github.io/activitylogs/static/js/2.a6f894ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a16e9b8287d82d7faad0",
+    "url": "https://RobertVo93.github.io/activitylogs/static/js/main.811a8946.chunk.js"
+  },
+  {
+    "revision": "baecbb6759ec45d8e277",
+    "url": "https://RobertVo93.github.io/activitylogs/static/js/runtime-main.3da36f8b.js"
+  }
+]);
